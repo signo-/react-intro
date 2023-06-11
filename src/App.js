c95d0df46aa97f32017d10f6cd290a5336450eb4
Mike Downer <mdowner6888@gmail.com>
2023-06-11 18:55:51 -0400

    Added static card

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import logo from './logo.svg';
+import { Button, Card, CardContent, Typography, CardActionArea, CardActions, CardMedia } from '@mui/material';
 import './App.less';
 
 function App() {
+
+  const generateCard = () => {
+    return(
+      <Card sx={{ maxWidth: 345 }}>
+        <CardMedia
+          component="img"
+          height="140"
+          image="/static/images/cards/contemplative-reptile.jpg"
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small" color="primary">
+            Add to cart
+          </Button>
+        </CardActions>
+      </Card>
+    )
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +38,7 @@ function App() {
       </header>
       <div className="main_body">
         <div>Hello World</div>
+        {generateCard()}
       </div>
     </div>
   );
