efdf369cbb8621683895104ba999abd4d3ac2fc6
Mike Downer <mdowner6888@gmail.com>
2023-06-11 17:56:43 -0400

    Updated project to use less

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.less';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <div className="main_body">
+        <div>Hello World</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
